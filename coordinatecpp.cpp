992a2911bf981c0bda4bc1e71d4e3165eab54cdf
Rustam97 <40730706+Rustam97@users.noreply.github.com>
2018-07-20 14:03:51 +0400

    Add files via upload

--- coordinatecpp.cpp
+++ coordinatecpp.cpp
@@ -12,37 +12,32 @@
 using namespace std;
 
 
-
-/////////////////////////////////////////////////////////
-double wgs84 (double Lat,double Long)
-{double re;       // экваториальный радиус(большая полуось)
-double e;       // эксцентриситет
-double Rmer;    // радиус меридиана
-double Rnorm;   // нормальный радиус (радиус кривизны вертикала)
-double Reqviv;  //локальный радиус Земли(геоида)
-double Lat;     //текущая широта в градусах
-double Long;    //текущая долгота в градусах
-double Latrad;  //перевод широты в радианы
-double Longrad; //перевод долготы в радианы
-double x;       //координата х
-double y;       //координата у
-double z;       //координата z
-re = 6378137;
-e = 0.081819190842622;
-Latrad = Lat*(180/M_PI);
-Longrad = Long*(180/M_PI);
-Rmer = (re*(1-pow(e,2)))/pow((1-pow(e,2)*pow(sin(Latrad),2)),3/2); 
-Rnorm = (re)/pow((1-pow(e,2)*pow(sin(Latrad),2)),1/2); 
-Reqviv = sqrt(Rmer*Rnorm);
-x = (Rnorm + Reqviv)*cos(Latrad)*cos(Longrad);
-y = (Rnorm + Reqviv)*cos(Latrad)*sin(Longrad);
-z = ((1-pow(e,2))*Rnorm+Reqviv)*sin(Latrad);
-double i[3] = {x, y, z};
+class wgs84
+{double re, e, Latrad, Longrad, Rmer,  Rnorm, Reqviv, x, y, z, Lat, Long;      //Lat - текущая широта в градусах; Long - текущая долгота в градусах;      
+public:      
+void get_res()
+{ cout << "Введите текущую широту в градусах"  << endl;
+cin >>  Lat;
+cout << "Введите текущую долготу в градусах"  << endl;
+cin >> Long;
+double re = 6378137;                  // экваториальный радиус(большая полуось)
+double e = 0.081819190842622;         // эксцентриситет
+double Latrad = Lat*(M_PI/180);       //перевод широты в радианы
+double Longrad = Long*(M_PI/180);     //перевод долготы в радианы
+double Rmer = (re*(1-pow(e,2)))/pow((1-pow(e,2)*pow(sin(Latrad),2)),3/2);    // радиус меридиана
+double Rnorm = (re)/pow((1-pow(e,2)*pow(sin(Latrad),2)),1/2);                // нормальный радиус (радиус кривизны вертикала)
+double Reqviv = sqrt(Rmer*Rnorm);     //локальный радиус Земли(геоида)
+double x = (Rnorm + Reqviv)*cos(Latrad)*cos(Longrad);     //координата х
+double y = (Rnorm + Reqviv)*cos(Latrad)*sin(Longrad);     //координата у
+double z = ((1-pow(e,2))*Rnorm+Reqviv)*sin(Latrad);       //координата z 
+double i[4] = {x, y, z, Reqviv};
 cout << "Координата X" << "		" << i[0] << endl;
 cout << "Координата Y" << "		" << i[1] << endl;
 cout << "Координата Z" << "		" << i[2] << endl;
-};
-/////////////////////////////////////////////////////////			
+cout << "Геодезическая высота H" << "		" << i[3] << endl;
+}
+}s1;
+		
 
 
 
@@ -50,20 +45,9 @@ int main()
 {  SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-/////////////////////////////////////////////////////////
-double Lat;    
-double Long;    
-/////////////////////////////////////////////////////////
-cout << "Введите текущую широту в градусах"  << endl;
-cin >>  Lat;
-cout << "Введите текущую долготу в градусах"  << endl;
-cin >> Long;
-/////////////////////////////////////////////////////////
-wgs84 (Lat, Long);
-/////////////////////////////////////////////////////////
+s1.get_res();
    system("pause");
      return 0;
-/////////////////////////////////////////////////////////
 
 }
 
